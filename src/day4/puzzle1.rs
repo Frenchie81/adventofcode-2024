d0898154e9fafe5541e5403c0680b9d04e1a511e
Brian French <bfrench9481@gmail.com>
2024-12-04 07:01:57 -0500

    day 4 puzzle 1 solved

---
+++ src/day4/puzzle1.rs
@@ -0,0 +1,141 @@
+use crate::read_file;
+
+pub fn solve_day4_puzzle1() -> i64 {
+    let contents = read_file("day4.txt");
+
+    get_result(contents.as_str())
+}
+
+fn get_result(contents: &str) -> i64 {
+    let mut count = 0_i64;
+
+    let mut grid = Vec::<Vec<char>>::new();
+    for line in contents.lines() {
+        grid.push(line.chars().collect::<Vec<char>>());
+    }
+
+    let mut x = 0;
+    let grid_len = grid.len();
+
+    while x < grid_len {
+        let mut y = 0;
+        let row_len = grid[x].len();
+        while y < row_len {
+            let temp = grid[x][y];
+            if temp == 'X' {
+                // right
+                if y < row_len - 3 {
+                    let word = [grid[x][y], grid[x][y + 1], grid[x][y + 2], grid[x][y + 3]];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // left
+                if y > 2 {
+                    let word = [grid[x][y], grid[x][y - 1], grid[x][y - 2], grid[x][y - 3]];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // up
+                if x > 2 {
+                    let word = [grid[x][y], grid[x - 1][y], grid[x - 2][y], grid[x - 3][y]];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // down
+                if (x + 3) < grid_len {
+                    let word = [grid[x][y], grid[x + 1][y], grid[x + 2][y], grid[x + 3][y]];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // left up
+                if x > 2 && y > 2 {
+                    let word = [
+                        grid[x][y],
+                        grid[x - 1][y - 1],
+                        grid[x - 2][y - 2],
+                        grid[x - 3][y - 3],
+                    ];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // left down
+                if (x + 3) < grid_len && y > 2 {
+                    let word = [
+                        grid[x][y],
+                        grid[x + 1][y - 1],
+                        grid[x + 2][y - 2],
+                        grid[x + 3][y - 3],
+                    ];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // right up
+                if x > 2 && y < row_len - 3 {
+                    let word = [
+                        grid[x][y],
+                        grid[x - 1][y + 1],
+                        grid[x - 2][y + 2],
+                        grid[x - 3][y + 3],
+                    ];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+                // right down
+                if x + 3 < grid_len && y < row_len - 3 {
+                    let word = [
+                        grid[x][y],
+                        grid[x + 1][y + 1],
+                        grid[x + 2][y + 2],
+                        grid[x + 3][y + 3],
+                    ];
+                    let word: String = word.iter().collect();
+                    if word == "XMAS" {
+                        count += 1;
+                    }
+                }
+            }
+
+            y += 1;
+        }
+        x += 1;
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn demo() {
+        let contents = r"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+        let result = get_result(contents);
+
+        assert_eq!(18, result);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::{env, fs, path::Path};
 use day1::{puzzle1::solve_day1_puzzle1, puzzle2::solve_day1_puzzle2};
 use day2::{puzzle1::solve_day2_puzzle1, puzzle2::solve_day2_puzzle2};
 use day3::{puzzle1::solve_day3_puzzle1, puzzle2::solve_day3_puzzle2};
+use day4::puzzle1::solve_day4_puzzle1;
 
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 fn read_file(file_name: &str) -> String {
     let input_path = env::var("INPUT_PATH").expect("INPUT_PATH environment variable must be set");
@@ -35,5 +37,8 @@ fn main() {
     println!("day3 puzzle1: {day3_puzzle1_result}");
 
     let day3_puzzle2_result = solve_day3_puzzle2();
-    println!("day3 puzzle1: {day3_puzzle2_result}");
+    println!("day3 puzzle2: {day3_puzzle2_result}");
+
+    let day4_puzzle1_result = solve_day4_puzzle1();
+    println!("day4 puzzle1: {day4_puzzle1_result}");
 }
